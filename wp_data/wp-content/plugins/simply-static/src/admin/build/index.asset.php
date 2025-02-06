716f60049dda998a6fb482ce5ae436699fb669fa
hugo <hugo@pc.n1>
2025-02-06 15:35:25 +0800

    Signed-off-by: hugo <hugo@pc.n1>

--- wp_data/wp-content/plugins/simply-static/src/admin/build/index.asset.php
+++ wp_data/wp-content/plugins/simply-static/src/admin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '17f1da66611772d62e3f');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => 'ae0c2b29f256b90e667a');
